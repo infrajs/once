14fec4d4511023f0c791f37b2c2e67b8dbf6bd9b
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-07 10:49:59 +0400

    fix

--- Once.js
+++ Once.js
@@ -1,38 +1,36 @@
-(function (){
-	
-	var Once = {
-		store: {},
-		omit: function(name, args) {
-			var hash = Once.key(name, args);
-			if (!Once.store[hash]) {
-				Once.store[hash] = {'result': 'is'};
-				return false; //будет означать пропустить в условии
+let Once = {
+	store: {},
+	omit: function (name, args) {
+		var hash = Once.key(name, args);
+		if (!Once.store[hash]) {
+			Once.store[hash] = { 'result': 'is' };
+			return false; //будет означать пропустить в условии
+		}
+		return true;
+	},
+	key: function (name, args) {
+		if (!name) name = '';
+		return name + '-' + JSON.stringify(args);
+	},
+	exec: function (name, call, args, re) {
+		var hash = Once.key(name, args);
+		if (!Once.store[hash] || re) {
+			if (typeof (args) == 'object' && args.constructor == Array) {
+				var r = call.apply(this, args.concat([re]));
+			} else {
+				var r = call.apply(this, [re]);
 			}
-			return true;
-		},
-		key: function (name, args) {
-			if (!name) name = '';
-			return name + '-' + Hash.exec(args);
-		},
-		exec: function (name, call, args, re) {
-			var hash = Once.key(name, args);
-			if (!Once.store[hash]||re) {
-				if (typeof(args)=='object'&&args.constructor==Array) {
-					var r=call.apply(this, args.concat([re]));
-				} else {
-					var r=call.apply(this, [re]);
-				}
-				Once.store[hash] = {
-					result:r
-				};
-			}
-			return Once.store[hash].result;
-		},
-		clear: function (name, args) {
-			var hash=Hash.exec(name, args);
-			delete Once.store[hash];
-			return hash;
-		}	
+			Once.store[hash] = {
+				result: r
+			};
+		}
+		return Once.store[hash].result;
+	},
+	clear: function (name, args) {
+		var hash = Once.key(name, args);
+		delete Once.store[hash];
+		return hash;
 	}
-	window.Once=Once;
-})();
+}
+window.Once = Once;
+export { Once };
